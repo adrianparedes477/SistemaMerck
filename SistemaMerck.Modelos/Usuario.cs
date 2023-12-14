cf20ebed04ce47680e08c4f62b34c555d6f50c04
adrianparedes477 <adrianjparedes477@gmail.com>
2023-12-14 18:27:42 -0300

    Cambios en el grafico para nueva version

--- SistemaMerck.Modelos/Usuario.cs
+++ SistemaMerck.Modelos/Usuario.cs
@@ -1,6 +1,4 @@
-﻿
-
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SistemaMerck.Modelos
 {
@@ -8,21 +6,29 @@ namespace SistemaMerck.Modelos
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "El campo Apellido es obligatorio.")]
         public string Apellido { get; set; }
 
+        [Required(ErrorMessage = "El campo Dirección es obligatorio.")]
         public string Direccion { get; set; }
 
+        [Required(ErrorMessage = "El campo Correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
         public string Correo { get; set; }
 
         [Display(Name = "Edad Actual")]
+        [Required(ErrorMessage = "El campo Edad Actual es obligatorio.")]
         public int EdadActual { get; set; }
+
         [Display(Name = "Edad Primera Menstruación")]
-        [Range(typeof(int), "0", "100", ErrorMessage = "La Edad Primera Menstruación no puede ser mayor que la Edad Actual.")]
+        [Required(ErrorMessage = "El campo Edad Primera Menstruación es obligatorio.")]
+        [Range(0, 100, ErrorMessage = "La Edad Primera Menstruación debe estar entre 0 y 100.")]
         public int EdadPrimeraMentruacion { get; set; }
+
         public double ReservaOvarica { get; set; }
-
-
     }
+
 }
---
+++ SistemaMerck/wwwroot/js/chartConfig.js
@@ -0,0 +1,40 @@
+﻿document.addEventListener('DOMContentLoaded', (event) => {
+    const chartDiv = document.getElementById('chart_div');
+    const edadActual = parseInt(chartDiv.dataset.edadActual);
+    const edadPrimeraMenstruacion = parseInt(chartDiv.dataset.edadPrimeraMenstruacion);
+
+    Highcharts.chart('chart_div', {
+        chart: {
+            type: 'line',
+            backgroundColor: '#fdddca' // Color de fondo estilo piel humana
+        },
+        title: {
+            text: 'Taza de fertilidad'
+        },
+        xAxis: {
+            categories: ['Edad Actual', 'Edad Primera Menstruación']
+        },
+        yAxis: {
+            title: {
+                text: 'Valor'
+            }
+        },
+        series: [{
+            name: 'Reserva Ovárica',
+            data: [edadActual, edadPrimeraMenstruacion]
+        }]
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
